7a6b887361777f0a0226021f164045078efc11ac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-15 15:59:37 +0200

    💬 add comment

--- src/components/hoc/with-additional-args/with-additional-args.tsx
+++ src/components/hoc/with-additional-args/with-additional-args.tsx
@@ -1,6 +1,28 @@
 import type { ElementType } from 'react';
 import type { AnyObject, VariableFC } from 'xenopomp-essentials';
 
+/**
+ * Adds additional args to pre-made component.
+ *
+ * @param comp
+ * @param fn
+ *
+ * @example
+ * const Comp = withAdditionalArgs<'button', { active?: boolean }>(
+ *   'button',
+ *   ({ Comp, active, className, ...props }) => (
+ *     <Comp
+ *       className={cn(
+ *         {
+ *           'bg-red-500': active,
+ *         },
+ *         className,
+ *       )}
+ *       {...props}
+ *     />
+ *   ),
+ * );
+ */
 export function withAdditionalArgs<
   TComp extends ElementType,
   TArgs extends AnyObject,
