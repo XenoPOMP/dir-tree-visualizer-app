185dc3d3d460435eab9207cab8cdb420c61d27eb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-23 15:13:00 +0200

    ⭕️ add loader

--- src/components/ui/ExportImageButton/ExportImageButton.tsx
+++ src/components/ui/ExportImageButton/ExportImageButton.tsx
@@ -1,24 +1,37 @@
+'use client';
+
 import cn from 'classnames';
 import { ImageDown } from 'lucide-react';
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 import useScreenshot from 'use-screenshot-hook';
 
-import { Button } from '@/components/ui/kit';
+import { Button, Loading } from '@/components/ui/kit';
 import type { IPreviewRef } from '@/types';
 
 export const ExportImageButton: FC<IPreviewRef> = ({ previewRef }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { takeScreenshot } = useScreenshot({
     ref: previewRef,
   });
 
   return (
-    <Button
-      variant='control'
-      square
-      className={cn('text-[--heading-2]')}
-      onClick={() => takeScreenshot()}
-    >
-      <ImageDown size='1em' />
-    </Button>
+    <>
+      <Button
+        variant='control'
+        square
+        className={cn('text-[--heading-2] disabled:hover:bg-block-hover')}
+        onClick={() => {
+          setIsLoading(true);
+
+          takeScreenshot().then(() => {
+            setIsLoading(false);
+          });
+        }}
+        disabled={isLoading}
+      >
+        {!isLoading ? <ImageDown size='1em' /> : <Loading variant='circle' />}
+      </Button>
+    </>
   );
 };
