c4d719aba2f83f76af349b922c7696a2f6ff37fa
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-23 11:36:59 +0200

    🍽️ add setter fn

--- src/zustand/stores/usePreviewSettings.ts
+++ src/zustand/stores/usePreviewSettings.ts
@@ -1,14 +1,26 @@
 import { create } from 'zustand';
 
-export interface IPreviewSettings {
+interface IDataSettings {
   folderIconSize: number;
   padding: number;
   showFolderGuides: boolean;
 }
 
+type OrPartial<T> = T | Partial<T>;
+type Payload =
+  | OrPartial<IDataSettings>
+  | ((partial: IDataSettings) => OrPartial<IDataSettings>);
+
+interface IMethods {
+  set: (partial: Payload) => void;
+}
+
+export type PreviewSettings = IDataSettings & IMethods;
+
 /** Setup preview image settings through all app. */
-export const usePreviewSettings = create<IPreviewSettings>(() => ({
+export const usePreviewSettings = create<PreviewSettings>(set => ({
   folderIconSize: 16,
   padding: 24,
   showFolderGuides: true,
+  set,
 }));
